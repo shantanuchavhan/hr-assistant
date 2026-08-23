dcf3c60c72728826a6c83a7b20b3f7c7ed8c5234
shantanuchavhan <shantanuchavhan10@gmail.com>
2026-08-23 23:05:05 +0530

    commit

--- main.py
+++ main.py
@@ -102,5 +102,5 @@ async def search_files(request:Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
